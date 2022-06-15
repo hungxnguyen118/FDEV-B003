8f464786459b00c0384dfc0860b87b25d6018ba8
Hung Nguyen <nxhung118@gmail.com>
2022-06-15 23:42:32 +0700

    animation slide left and right

--- FDEV-D001-main/src/App.js
+++ FDEV-D001-main/src/App.js
@@ -1,28 +1,84 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Widgets/Header';
-import Content from './Widgets/Content';
-import Footer from './Widgets/Footer';
+import { useEffect, useState } from 'react';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+//import socketIOClient from 'socket.io-client';
+
 import DragAndDrop from './Modules/DragAndDrop';
-import GameEvent from './Modules/GameEvent';
-import RegisterForm from './Modules/RegisterForm';
+import TrangChu from './Pages/TrangChu';
+//import Register from './Pages/Register';
+//import Game from './Pages/Game';
+import DetailProduct from './Pages/DetailProduct';
 
-import { BrowserRouter as Router } from 'react-router-dom'
+import Header from "./Widgets/Header";
+import Footer from './Widgets/Footer';
 
+const ENDPOINT = 'http://localhost:4000'
 
-function App() {
+function Test() {
+
+  // const [response, setResponse] = useState("");
+  const [itemactive, setItemActive] = useState(0);
+  const [classactive, setClassActive] = useState(0);
+  const number_item = 6
+
+  // useEffect(() => {
+  //   const socket = socketIOClient(ENDPOINT);
+  //   socket.on("hello", data => {
+  //     setResponse(data);
+  //   });
+  // }, []);
+
+  useEffect(() => {
+    setInterval(() => {
+      setClassActive('slideInRight');
+      setTimeout(() => {
+        setClassActive('slideInLeft');
+      }, 2000);
+    }, 4000)
+  })
+
   return (
-    <div>
-      <Router>
-        <Header></Header>
-        <Content></Content>
-        {/* <RegisterForm /> */}
-        <Footer></Footer>
-        {/* <DragAndDrop /> */}
-        {/* <GameEvent /> */}
-      </Router>
-    </div>
+    // <p>
+    //   It's <time dateTime={response}>{response}</time>
+    // </p>
+    <>
+      {itemactive}
+      <div className="ds_slide">
+        <div className="show_slide">
+          <div className={'group_item group_item_1 active ' + classactive}>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+          </div>
+        </div>
+      </div>
+    </>
   );
+
+  // return (
+  //   <div className="App">
+  //     <BrowserRouter>
+  //       <Header />
+  //       <Routes>
+  //         <Route path="/" element={<TrangChu/>}></Route>
+  //         <Route path="/register" element={<Register />}></Route>
+  //         <Route path="/game" element={<Game />}></Route>
+  //         <Route path="/draganddrop" element={<DragAndDrop />}></Route>
+  //         <Route path="/product">
+  //           <Route path=":productId" element={<DetailProduct />} />
+  //         </Route>
+  //       </Routes>
+  //       <Footer />
+  //     </BrowserRouter>
+  //   </div>
+  // );
 }
 
-export default App;
+export default Test;
